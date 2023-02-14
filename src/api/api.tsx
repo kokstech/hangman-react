395eb813f5b6b62eeb4a9babc912e98e68fcf646
kokstech <stefan.ristic.koki@gmail.com>
2023-02-14 17:49:05 +0100

    dotenv second commit

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,7 +4,6 @@ export const movies: string[] = [];
 
 export async function getMovies() {
   await axios.get(env.BACKEND_URL).then((res) => {
-    console.log(process.env.BACKEND_URL);
     res.data.map((movie: any) => movies.push(movie.title));
   });
 }
