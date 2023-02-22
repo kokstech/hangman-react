2229cca9ddb6236cac61048693efabe9ba5f643b
kokstech <74383110+kokstech@users.noreply.github.com>
2023-02-22 16:33:10 +0100

    Update api.tsx

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,7 +3,7 @@ import env from "react-dotenv";
 export const movies: string[] = [];
 
 export async function getMovies() {
-  await axios.get(process.env.BACKEND_URL).then((res) => {
+  await axios.get("https://hangman-backend.onrender.com/api").then((res) => {
     res.data.map((movie: any) => movies.push(movie.title));
   });
 }
